22258fab024ae205f7d7c6fee82a76e9ab0c1120
Jlde54 <jlde54@sfr.fr>
2023-09-27 11:31:09 +0200

    add page "error"

--- src/pages/erreur.jsx
+++ src/pages/erreur.jsx
@@ -1,7 +1,19 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
 import '../styles/pages/erreur.css'
 
 function Erreur() {
-    return <div className="apropos">Erreur</div>
-  }
+  return (
+    <div className="error">
+      <h1 className="error-title">{404}</h1>
+      <h2 className="error-subtitle">
+        Oups! La page que vous demandez n'existe pas.
+      </h2>
+      <h3 className="error-link">
+        <NavLink to="/">Retourner sur la page d'accueil</NavLink>
+      </h3>
+    </div>
+  )
+}
   
-  export default Erreur
+export default Erreur
